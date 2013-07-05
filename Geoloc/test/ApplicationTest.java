a6b11954259c6454b9e0b101cf9995ac7dd7e2f5
jdaros <daros.jonathan@gmail.com>
2013-07-05 19:13:00 +0200

    first commit.

---
+++ Geoloc/test/ApplicationTest.java
@@ -0,0 +1,34 @@
+
+import org.junit.*;
+
+import play.mvc.Result;
+
+import static org.fest.assertions.Assertions.*;
+
+import static play.test.Helpers.GET;
+import static play.test.Helpers.*;
+
+/**
+*
+* Simple (JUnit) tests that can call all parts of a play app.
+* If you are interested in mocking a whole application, see the wiki for more details.
+*
+*/
+public class ApplicationTest {
+
+//    @Test 
+//    public void simpleCheck() {
+//        int a = 1 + 1;
+//        assertThat(a).isEqualTo(2);
+//    }
+    
+    @Test
+    public void renderTemplate() {
+//        Content html = views.html.index.render("Your new application is ready.");
+//        assertThat(contentType(html)).isEqualTo("text/html");
+//        assertThat(contentAsString(html)).contains("Your new application is ready.");
+    }
+
+
+   
+}
